9eea4d2210271e7432334a50af330ab9fddcdf3d
dam22-20 <dam22-20@c16x16.iescierva.net>
2024-01-30 09:27:39 +0100

    Mostrar mi nombre

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,6 +1,7 @@
 public class ejemplo1{
-public static void main(String[]args){
-System.out.println("hola mundo");
-
-}
+    public static void main(String[]args){
+        System.out.println("hola mundo");
+        String nombre = José;
+        System.out.println("Mi nombre es: "+nombre);
+    }
 }
